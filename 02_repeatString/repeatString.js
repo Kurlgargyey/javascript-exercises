91d46a453e41b32b249dcda8936271e80ba011b6
Kurlgargyey <nahsirkivar@gmail.com>
2022-01-27 14:35:34 +0100

    02_repeatString done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, loops) {
+ let i = 0;
+ let result = '';
+ while(i<loops){
+  result += string;
+  i++;
+ }
+ if(loops < 0){
+  return 'ERROR';
+ }
+ return result;
 };
 
 // Do not edit below this line
