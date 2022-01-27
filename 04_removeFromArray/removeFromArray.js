3e99a16ba3c58e0f29b8d32a77aeb9b7457d5d82
Kurlgargyey <nahsirkivar@gmail.com>
2022-01-27 15:40:28 +0100

    solved 04 with .filter method (maybe not intended)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
 
+const removeFromArray = function(array, ...args) {
+ filter = args;
+ return array.filter((item)=>{
+  for (argument of filter) {
+   if (item === argument) {
+    return false;
+   }
+  }
+  return true;
+ })
 };
 
 // Do not edit below this line
