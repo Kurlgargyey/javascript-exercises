bc2aeadb25b03d3ec35ff2cd979aed2475c04825
Kurlgargyey <nahsirkivar@gmail.com>
2022-01-27 15:14:03 +0100

    03_reverseString completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+ let result = '';
+ let i = string.length;
+ for (; i >= 0 ; i--){
+  const extractedLetter = string.slice(i-1,i);
+  result += extractedLetter;
+ }
+ return result;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
