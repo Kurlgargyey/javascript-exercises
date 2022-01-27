dbc16d04ba2dfd015255b9eda972b1cd01674e74
Kurlgargyey <nahsirkivar@gmail.com>
2022-01-27 16:10:15 +0100

    solved 05_sumAll (bit ugly)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+ let result = 0;
+ let start = 0;
+ let end = 0;
+ if (num1 > num2) {
+  end = num1;
+  start = num2;
+ }
+ else {
+  end = num2;
+  start = num1;
+ }
+ if ((typeof start) != "number" || (typeof end) != "number" || start < 0 || end < 0) {
+  return "ERROR";
+ }
+ for (let i = start; i <= end; i++){
+  result += i;
+ }
+ return result;
 };
 
 // Do not edit below this line
