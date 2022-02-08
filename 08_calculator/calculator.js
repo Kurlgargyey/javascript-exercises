c14c0dc2743a7af011a09e61de08b607dd8f4581
Kurlgargyey <nahsirkivar@gmail.com>
2022-02-08 17:02:33 +0100

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	result = arr.reduce((total, num) => {
+    total += num;
+    return total;
+  }, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  result = arr.reduce((total, num) => {
+    total *= num;
+    return total;
+  }, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	start = 1;
+  total = 1;
+  while (start <= num) {
+    total *= start;
+    start ++;
+  }
+  return total;
 };
 
 // Do not edit below this line
