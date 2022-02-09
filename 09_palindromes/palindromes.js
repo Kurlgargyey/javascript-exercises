63c2cd2a74106bec6886200fb8bbfd19a737723e
Kurlgargyey <nahsirkivar@gmail.com>
2022-02-09 14:06:53 +0100

    palindromes done (looked up regex)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+ const punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+ let cleanstring = string.replace(punctuation, '');
+ cleanstring = cleanstring.replace(/ /g, '');
+ cleanstring = cleanstring.toLowerCase();
+ let i = 0;
+ while (i <= Math.floor(cleanstring.length/2)){
+  if (cleanstring[i] === cleanstring[cleanstring.length-i-1]) {
+   i++;
+  } else {
+   return false;
+  }
+ }
+ return true;
 };
 
 // Do not edit below this line
