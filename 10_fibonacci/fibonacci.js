94d3a4f9e82169a83a782bfea8ec375e7a0236e6
Kurlgargyey <nahsirkivar@gmail.com>
2022-02-09 14:28:04 +0100

    fibonacci solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+ let fibstart = [1,1]
+ if (num > 0) {
+  while (fibstart.length <= num){
+   fibstart.splice(fibstart.length,0,fibstart[fibstart.length-1]+fibstart[fibstart.length-2])
+  }
+  return fibstart[num-1]
+ } else {
+  return "OOPS"
+ }
 };
 
 // Do not edit below this line
