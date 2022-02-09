d514a52d388af812ee00a30af6e49da40b41d122
Kurlgargyey <nahsirkivar@gmail.com>
2022-02-09 14:47:08 +0100

    solved findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+ const oldest = arr.reduce(function(obj, person){
 
+  const personStart = person.yearOfBirth;
+
+  if (!person.yearOfDeath) {
+   const personEnd = (new Date()).getFullYear(); 
+   person.age = personEnd-personStart; 
+  } 
+  
+  else {
+   const personEnd = person.yearOfDeath;
+   person.age = personEnd-personStart;
+  }
+
+  if (person.age > obj.age) {
+   obj = person;
+  }
+  
+  return obj;
+
+ },
+  {
+   name : '',
+   yearOfBirth: 0,
+   yearOfDeath: 0,
+   age: 0,
+   }
+ )
+
+ return oldest;
 };
 
 // Do not edit below this line
